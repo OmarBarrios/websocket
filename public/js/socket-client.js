3d373a740254896c2620f9e6e55b927b2de3b4c2
omar.barrios <omar.barrios@alumnos.uneatlantico.es>
2021-09-05 00:43:43 +0200

    [develop] sockets project

---
+++ public/js/socket-client.js
@@ -0,0 +1,43 @@
+
+
+// referencias HTML
+const lblOnline =  document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline'); 
+const txtMensaje =  document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar'); 
+
+const socket = io();
+
+
+socket.on('connect', () => {
+    //console.log('cliente conectado');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+})
+
+socket.on('disconnect', () => {
+    //console.log('cliente desconectado');
+
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+})
+socket.on('enviar-mensaje', (payload) => {
+    console.log(payload);
+
+})
+
+
+btnEnviar.addEventListener('click', () =>{
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '1234qe123',
+        fecha: new Date().getTime()
+    }
+    
+    socket.emit('enviar-mensaje', payload, (id) =>{
+        console.log('desde el server', id );
+    });
+
+})
